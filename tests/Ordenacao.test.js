9ddb254abeff3136e65ddbd202679ccc50edf504
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-11-28 22:29:59 -0300

    Ajuste teste Ordenacao

--- tests/Ordenacao.test.js
+++ tests/Ordenacao.test.js
@@ -5,13 +5,13 @@ var items2 = [10,3,5,1,2,9];
 var items3 = [8,4,12,15,2,1];
 
 test('O conjunto deve ser organizado em ordem QuickSort', () => {
-    expert(quickSort(items1, 0, items.length - 1)).toMatchObjetct([2,3,5,6,7,9]);
+    expect(quickSort(items1, 0, items1.length - 1)).toMatchObject([2,3,5,6,7,9]);
 });
 
 test('O conjunto deve ser organizado em ordem QuickSort', () => {
-    expert(quickSort(items2, 0, items.length - 1)).toMatchObjetct([10,3,5,1,2,9]);
+    expect(quickSort(items2, 0, items2.length - 1)).toMatchObject([1,2,3,5,9,10]);
 });
 
 test('O conjunto deve ser organizado em ordem QuickSort', () => {
-    expert(quickSort(items3, 0, items.length - 1)).toMatchObjetct([8,4,12,15,2,1]);
+    expect(quickSort(items3, 0, items3.length - 1)).toMatchObject([1,2,4,8,12,15]);
 });

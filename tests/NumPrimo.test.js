13763355e67ed96e4543f44021b936b06a077435
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-11-28 22:29:25 -0300

    Ajustes teste Numero Primo

--- tests/NumPrimo.test.js
+++ tests/NumPrimo.test.js
@@ -1,13 +1,13 @@
 const validarNumPrimo = require('../funcoes/NumPrimo');
 
 test('O valor 7 e um numero primo', () => {
-    expert(validarNumPrimo(7)).toMatchObjetct(["O numero 7 e primo"]);
+    expect(validarNumPrimo(7)).toMatchObject(["O numero 7 e primo"]);
 });
 
 test('O valor 19 e um numero primo', () => {
-    expert(validarNumPrimo(19)).toMatchObjetct(["O numero 19 e primo"]);
+    expect(validarNumPrimo(19)).toMatchObject(["O numero 19 e primo"]);
 });
 
 test('O valor 15 nao e um numero primo', () => {
-    expert(validarNumPrimo(15)).toMatchObjetct(["O numero 15 nao e primo"]);
+    expect(validarNumPrimo(15)).toMatchObject(["O numero 15 nao e primo"]);
 });

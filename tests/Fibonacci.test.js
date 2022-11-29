7d14975dc47b952dbc9cc68c4f97b45dc2d752e6
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-11-28 22:28:46 -0300

    Ajuste Teste Fibonacci

--- tests/Fibonacci.test.js
+++ tests/Fibonacci.test.js
@@ -1,13 +1,13 @@
 const fibonacci = require('../funcoes/Fibonacci');
 
-test('A seguencia fibonacci deve ser 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377', () => {
-    expert(fibonacci(15)).toMatchObjetct([0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]);
+test('A sequencia fibonacci deve ser 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377', () => {
+    expect(fibonacci(15)).toMatchObject(["0,1,1,2,3,5,8,13,21,34,55,89,144,233,377"]);
 });
 
-test('A seguencia fibonacci deve ser 0,1,1,2,3,5,8,13', () => {
-    expert(fibonacci(8)).toMatchObjetct([0,1,1,2,3,5,8,13]);
+test('A sequencia fibonacci deve ser 0,1,1,2,3,5,8,13', () => {
+    expect(fibonacci(8)).toMatchObject(["0,1,1,2,3,5,8,13"]);
 });
 
-test('A seguencia fibonacci deve ser 0,1,1,2', () => {
-    expert(fibonacci(4)).toMatchObjetct([0,1,1,2]);
+test('A sequencia fibonacci deve ser 0,1,1,2', () => {
+    expect(fibonacci(4)).toMatchObject(["0,1,1,2"]);
 });

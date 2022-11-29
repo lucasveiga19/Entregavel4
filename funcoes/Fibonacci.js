0361228d71e2c664230c127a5437f024b2da770c
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-11-28 21:27:58 -0300

    Ajuste Fibonacci

--- funcoes/Fibonacci.js
+++ funcoes/Fibonacci.js
@@ -6,24 +6,28 @@ function fibonacci(quantidadeNumero){
   var termo2 = 1;
   var termo3;
   
-  var resultado = "";
+  var resultado = [];
   
-  resultado+= termo1+", "+termo2;
+  resultado+= [termo1, termo2];
   
-  if(resultado==0){
-    resultado == termo1;
-  } else if(resultado==1){
-    resultado == termo1+", "+termo2;
+  if(quantidadeNumero <= 0){
+    return [];
+  } else if (quantidadeNumero == 1){
+    resultado = [termo1];
+    return resultado;
+  } else if (quantidadeNumero == 2){
+    resultado = [termo1, termo2];
+    return resultado;
   }
   
   for(i=3; i<=quantidadeNumero; i++){
       termo3 = termo1+termo2;
-      resultado+=", "+termo3;
+      resultado+= [,termo3];
       termo1 = termo2;
       termo2 = termo3;
   }
   
-  return resultado;
+  return [resultado];
   
 }
 
